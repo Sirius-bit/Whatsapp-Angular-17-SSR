29c76f83f1d94cc9a59e52757f00ed31f62558ba
Sirius-bit <martellucci.ivo@gmail.com>
2023-12-12 02:03:50 +0100

    commit2

--- src/app/environments/envirionments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    linkForImgToSend: "../../assets/gif-img-for-chat/",
-    linkDefaultForImg: "../../assets/img-user/",
-    imgUserDefault: "../../assets/img-user/user-solid.svg",
-    keyEmoji: "0e376f1f496b114f6d021931b08bbf4e4234534a",
-    basicUrlEmoji: "https://emoji-api.com/emojis",
-    searchEmojiByString: "https://emoji-api.com/emojis?search=",
-    emojisInCategory: "https://emoji-api.com/categories/"
-}
